8850085e5529b70c8816b5100ddeca384c91337b
Artem Dogadailo <artem.dogadailo@student.griffith.ie>
2025-02-19 19:27:41 +0000

    adding functionality to the gradesMax gradesTotal gradesAverage countFails methods

--- src/griffith/Grades.java
+++ src/griffith/Grades.java
@@ -4,21 +4,39 @@ public class Grades {
 
 	// return max grades
 	public int gradesMax(int[] grades) {
-		return 0;
+		int max = Integer.MIN_VALUE;
+	    for (int grade : grades) {
+	        if (grade > max) {
+	            max = grade;
+	        }
+	    }
+	    return max;
 	}
 
 	// get the sum of array
 	public int gradesTotal(int[] grades) {
-		return 0;
+		int sum = 0;
+	    for (int grade : grades) {
+	        sum += grade;
+	    }
+	    return sum;
 	}
 	
 	// get average value of an array
 	public double gradesAverage(int[] grades) {
-		return 0;
+		//checking the grades length to avoid dividing by 0
+		if (grades.length == 0) return 0; 
+	    return (double) gradesTotal(grades) / grades.length;
 	}
 	
 	// count how many grades less than the minimum grade
 	public int countFails(int[] grades, int minGrade) {
-		return 0;
+		 int count = 0;
+		    for (int grade : grades) {
+		        if (grade < minGrade) {
+		            count++;
+		        }
+		    }
+		    return count;
 	}
 }
