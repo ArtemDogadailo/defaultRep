73f0a364d05b0384eedecb4fe9c098b2f127bb2d
Artem Dogadailo <artem.dogadailo@student.griffith.ie>
2025-02-19 19:10:52 +0000

    creating JUnit test file and Grades class

---
+++ src/griffith/Grades.java
@@ -0,0 +1,24 @@
+package griffith;
+
+public class Grades {
+
+	// return max grades
+	public int gradesMax(int[] grades) {
+		return 0;
+	}
+
+	// get the sum of array
+	public int gradesTotal(int[] grades) {
+		return 0;
+	}
+	
+	// get average value of an array
+	public double gradesAverage(int[] grades) {
+		return 0;
+	}
+	
+	// count how many grades less than the minimum grade
+	public int countFails(int[] grades, int minGrade) {
+		return 0;
+	}
+}
---
+++ src/griffith/GradesTest.java
@@ -0,0 +1,48 @@
+package griffith;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.Assert.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class GradesTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGradesMax() {
+		
+	}
+
+	public void testGradesTotal() {
+		
+	}
+
+	public void testGradesAverage() {
+		
+	}
+
+	public void testCountFails() {
+		
+	}
+	
+}
