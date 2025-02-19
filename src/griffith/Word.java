a755c991ae3db10227dee0344ebd4a04083240af
Artem Dogadailo <artem.dogadailo@student.griffith.ie>
2025-02-19 19:54:39 +0000

    adding stub methods to the word.java

--- src/griffith/Word.java
+++ src/griffith/Word.java
@@ -4,5 +4,26 @@
 package griffith;
 
 public class Word {
+	//adding an attrubute letters
+	private char[] letters;
 
+    // creating constructor
+    public Word(char[] letters) {
+        this.letters = letters;
+    }
+
+    // Checks if the word contains a given character
+    public boolean contains(char symbol) {
+        return false; 
+    }
+
+    // Returns the length of the word
+    public int length() {
+        return 0; 
+    }
+
+    // Returns the letters array
+    public char[] getLetters() {
+        return null; 
+    }
 }
